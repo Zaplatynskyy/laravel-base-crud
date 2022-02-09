f0845c3b0bba1af65be0094da04af5f8d3c15efc
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-09 17:55:07 +0100

    init method show, page show.blade.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,5 +5,29 @@
 @endsection
 
 @section('PageContent')
-    @dump($comics)
+    <div class="container">
+        <h1 class="title my-4 text-uppercase">Comics List</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Prezzo</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($comics as $comic)
+                <tr>
+                    <th>{{$comic->id}}</th>
+                    <td><a href="{{route('comics.show',$comic->id)}}"><strong>{{$comic->title}}</strong></a></td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>{{$comic->price}} $</td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+    </div>
 @endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.base')
+
+@section('PageTitle')
+    {{$comic->title}}
+@endsection
+
+@section('PageContent')
+    <div class="container">
+        <div class="card m-5 p-4">
+
+            <h2 class="title_comic mb-3 text-center">{{$comic->title}}</h2>
+
+            <div class="d-flex">
+                <img class="mr-3" src="{{$comic->thumb}}" alt="{{$comic->title}}">
+                <div class="info_comic">
+                    <p>{{$comic->description}}</p>
+                    <div class="info_sale">
+                        <span class="font-weight-bold">{{$comic->series}}</span>
+                        <span>{{$comic->sale_date}}</span>
+                        <div class="font-weight-bold">{{$comic->price}} $</div>
+                        <a href="{{route('comics.index')}}">
+                            <button type="button" class="btn btn-primary">Indietro</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
