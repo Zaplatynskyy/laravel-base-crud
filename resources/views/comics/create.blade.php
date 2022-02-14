5b4f9cd2665a85a6f7d5747a6b7083aabcac2994
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-14 17:31:39 +0100

    repopulating form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,7 +12,7 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title')}}">
                 @error('title')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -20,7 +20,7 @@
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" placeholder="Inserisci una descrizione" id="description" style="height: 100px" name="description"></textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" placeholder="Inserisci una descrizione" id="description" style="height: 100px" name="description">{{ old('description')}}</textarea>
                 @error('description')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -28,7 +28,7 @@
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Url Immagine</label>
-                <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb')}}">
                 @error('thumb')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -36,7 +36,7 @@
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="number" class="form-control @error('price') is-invalid @enderror" step="0.01" id="price" name="price">
+                <input type="number" class="form-control @error('price') is-invalid @enderror" step="0.01" id="price" name="price" value="{{ old('price')}}">
                 @error('price')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -44,7 +44,7 @@
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series')}}">
                 @error('series')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -52,7 +52,7 @@
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data</label>
-                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date">
+                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ old('date')}}">
                 @error('sale_date')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -60,7 +60,7 @@
 
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
-                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{ old('type')}}">
                 @error('type')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
